c1bcc689c605d9f290b1baeb16b659ddbd4e21b0
oxalcvasquez <oxalc0101@gmail.com>
2024-04-29 13:09:46 -0500

    Adding UI Update

--- components/CustomerCard.tsx
+++ components/CustomerCard.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import {View, Text, Button, Colors, Card} from 'react-native-ui-lib';
+import React, { useState } from 'react';
+import {View, Text, Button, Colors, Card, Modal} from 'react-native-ui-lib';
 import { ICustomer } from '../types/customer';
+import { StyleSheet, TextInput  } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 
 
 interface CustomerCardProps {
@@ -12,18 +14,97 @@ export const CustomerCard: React.FC<CustomerCardProps> = ({
   customer,
   onDelete,
 }) => {
+
+   const [nombres, setNombres] = useState(customer.name);
+   const [apellidos, setApellidos] = useState(customer.last_name);
+   const [correo, setCorreo] = useState(customer.email);
+   const [telefono, setTelefono] = useState(customer.phone);
+   //const [tipoCliente, setTipoCliente] = useState({id: 0, type: ''});
+   //const [types, setTypes] = useState<IType[]>([]);
+   const [estado, setEstado] = useState(customer.status);
+
+  const [openEditModal,setOpenEditModal] = useState(false);
+
   const handleDeleteCustomer = () => {
     onDelete(customer.id);
   };
+  const handleUpdateCustomer = () => {
+    setOpenEditModal(false);
+  };
 
+  const handleOpenModal = () => {
+    setOpenEditModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenEditModal(false);
+  };
   return (
     <Card flex-1 row spread margin-10 padding-10 key={customer.id}>
+      <Modal visible={openEditModal}>
+        <Card margin-10 padding-10>
+          <Text>Actualizar clientes</Text>
+          <TextInput
+            value={nombres}
+            onChangeText={setNombres}
+            placeholder="Nombres"
+            style={styles.input}
+          />
+          <TextInput
+            value={apellidos}
+            onChangeText={setApellidos}
+            placeholder="Apellidos"
+            style={styles.input}
+          />
+          <TextInput
+            value={correo}
+            onChangeText={setCorreo}
+            placeholder="Correo"
+            style={styles.input}
+          />
+          <TextInput
+            value={telefono}
+            onChangeText={setTelefono}
+            placeholder="Telefono"
+            style={styles.input}
+          />
+
+          <View row padding-10 style={styles.text}>
+            <Text>Estado</Text>
+            <CheckBox
+              value={estado}
+              onValueChange={status => setEstado(status)}
+              onCheckColor={Colors.purple30}
+            />
+          </View>
+          <View row center>
+            <Button
+              margin-10
+              label="Cancelar"
+              backgroundColor={Colors.grey40}
+              borderRadius={10}
+              onPress={handleCloseModal}
+            />
+            <Button
+              margin-10
+              label="Actualizar"
+              backgroundColor={Colors.purple30}
+              borderRadius={10}
+              onPress={handleOpenModal}
+            />
+          </View>
+        </Card>
+      </Modal>
       <View flex-1>
         <Text text60 color={Colors.blue50}>
           {customer.name} {customer.last_name}
         </Text>
-        <Text color={Colors.grey30} text70BO>{customer.email}</Text>
-        <Text color={Colors.grey40} text80>{customer.phone}</Text>
+        <Text color={Colors.grey30} text70BO>
+          {customer.email}
+        </Text>
+        <Text color={Colors.grey40} text80>
+          {customer.phone}
+        </Text>
         <Text>{customer.status}</Text>
         <Text>{customer.type.type}</Text>
       </View>
@@ -33,7 +114,7 @@ export const CustomerCard: React.FC<CustomerCardProps> = ({
           label="Editar"
           backgroundColor={Colors.$outlineWarning}
           borderRadius={10}
-          onPress={handleDeleteCustomer}
+          onPress={handleOpenModal}
         />
         <Button
           label="Eliminar"
@@ -45,4 +126,20 @@ export const CustomerCard: React.FC<CustomerCardProps> = ({
     </Card>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: 'white',
+    borderColor: Colors.purple30,
+  },
+  text: {
+    alignItems: 'center',
+  },
+});
+
 export default CustomerCard;
